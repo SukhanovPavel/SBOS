4c0d0f02874f0d7b5e64df26508c61c34371b035
Pavel Sukhanov <psuhanov77@gmail.com>
2023-04-29 21:40:41 +0300

    fix: scroll, styles

--- src/components/ScrollButton/ScrollButton.tsx
+++ src/components/ScrollButton/ScrollButton.tsx
@@ -5,32 +5,36 @@ import styles from "./ScrollButton.module.css";
 export const ScrollButton = () => {
 
     const [visible, setVisible] = useState(false);
+    const [scroll, setScroll] = useState(1);
 
-    const toggleVisible = () => {
-        const scrolled = document.documentElement.scrollTop;
-        if (scrolled > 300){
-            setVisible(true)
-        }
-        else if (scrolled <= 300){
-            setVisible(false)
-        }
-    };
+    useEffect(() => {
+        const toggleVisible = () => {
+            const scrolled = document.documentElement.scrollTop;
+            if (scrolled > 300){
+                setVisible(true)
+            }
+            else if (scrolled <= 300){
+                setVisible(false)
+            }
+        };
 
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-            /* you can also use 'auto' behaviour
-               in place of 'smooth' */
-        });
-    };
-
-    useEffect(() => window.addEventListener('scroll', toggleVisible));
+        // const scrollToTop = () => {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+                /* you can also use 'auto' behaviour
+                   in place of 'smooth' */
+            });
+        // };
+        window.addEventListener('scroll', toggleVisible);
+    }, [scroll]);
 
     return (
         <button className={styles._}>
-            <FaArrowCircleUp onClick={scrollToTop}
-                             style={{display: visible ? 'inline' : 'none'}} />
+            <FaArrowCircleUp
+                onClick={() => setScroll(scroll + 1)}
+                style={{display: visible ? 'inline' : 'none'}}
+            />
         </button>
     );
 }
--- src/pages/contacts.tsx
+++ src/pages/contacts.tsx
@@ -5,9 +5,10 @@ import {useState} from "react";
 import Head from "next/head";
 import {Header} from "@/components/Header/Header";
 import {Menu} from "@/components/Menu/Menu";
+import {Contacts} from "@/components/Contacts/Contacts";
 
 
-const Contacts = () => {
+const contacts = () => {
 
     const [modal, openModal] = useState(false);
 
@@ -23,43 +24,10 @@ const Contacts = () => {
             </Head>
             <Header handleClick={() => openModal(true)}/>
             <Menu />
-            <main className={styles.main}>
-                <div className={styles._}>
-                    <div className={styles.block}>
-                        <h1>Контакты</h1>
-                        <div className={styles.contact}>
-                            <p>Адрес:</p>
-                            <p>Шатура, проспект Ильича д.40</p>
-                        </div>
-                        <div className={styles.contact}>
-                            <p>Телефон:</p>
-                            <a href="tel:+79773716488">8 (977) 371-64-88</a>
-                        </div>
-                        <div className={styles.contact}>
-                            <p>Режим работы:</p>
-                            <div>Пн-пт: 9:00-18:00</div>
-                        </div>
-                        <div className={styles.contact}>
-                            <p>Email:</p>
-                            <a href="mailto:mail@sbos.ru">mail@sbos.ru</a>
-                        </div>
-                        <button onClick={() => openModal(true)}>Обратный звонок</button>
-                    </div>
-                    <div className={styles.block}>
-                        <Map
-                            className={styles.map}
-                            defaultState={{center: [55.575271, 39.529011], zoom: 15}}
-                            width="100%"
-                            height="75%"
-                        >
-                            <Placemark defaultGeometry={[55.575271, 39.529011]}/>
-                        </Map>
-                    </div>
-                </div>
-            </main>
+            <Contacts handleClick={() => openModal(true)} />
             {modal ? <Modal handleCloseModal={() => openModal(false)}/> : null}
         </YMaps>
     );
 }
 
-export default Contacts;
+export default contacts;
