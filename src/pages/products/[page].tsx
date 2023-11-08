c4c92758fead8176d446f61865925c9d91c1ac8a
Pavel Sukhanov <psuhanov77@gmail.com>
2023-11-08 16:36:54 +0300

    fix

--- src/pages/products/[page].tsx
+++ src/pages/products/[page].tsx
@@ -1,4 +1,5 @@
-import {useState} from "react";
+'use client'
+import {useEffect, useState} from "react";
 import Layout from "@/components/Layout/Layout";
 import {ProductCard} from "@/components/ProductCard/ProductCard";
 import {Modal} from "@/components/Modal/Modal";
@@ -12,23 +13,29 @@ export default function Page() {
 
     const router = useRouter();
 
-    let path = router.query.page;
 
-    return (
-        <>
-            <Layout meta={PRODUCT_DATA[path].title}/>
-            <ProductCard
-                title={PRODUCT_DATA[path].title}
-                image={PRODUCT_DATA[path].image}
-                price={PRODUCT_DATA[path].price}
-                description={PRODUCT_DATA[path].description}
-                title1={PRODUCT_DATA[path].title1}
-                list1={PRODUCT_DATA[path].list1}
-                title2={PRODUCT_DATA[path].title2}
-                list2={PRODUCT_DATA[path].list2}
-                title3={PRODUCT_DATA[path].title3}
-                list3={PRODUCT_DATA[path].list3}
-            />
-            {modal ? <Modal handleCloseModal={() => openModal(false)}/> : null}
-        </>
-    )};
+    const path = router.query.page;
+
+    if (typeof path === "string") {
+        return (
+            <>
+                <Layout meta={PRODUCT_DATA[path].title}/>
+                <ProductCard
+                    title={PRODUCT_DATA[path].title}
+                    image={PRODUCT_DATA[path].image}
+                    price={PRODUCT_DATA[path].price}
+                    description={PRODUCT_DATA[path].description}
+                    title1={PRODUCT_DATA[path].title1}
+                    list1={PRODUCT_DATA[path].list1}
+                    title2={PRODUCT_DATA[path].title2}
+                    list2={PRODUCT_DATA[path].list2}
+                    title3={PRODUCT_DATA[path].title3}
+                    list3={PRODUCT_DATA[path].list3}
+                />
+                {modal ? <Modal handleCloseModal={() => openModal(false)}/> : null}
+            </>
+        )
+    } else {
+        return null;
+    }
+};
