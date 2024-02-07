e8b5a78427f9edac3e0ad98bd9eeaaba8e4cb24b
Pavel Sukhanov <psuhanov77@gmail.com>
2024-02-07 12:14:05 +0300

    edit fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,31 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app';
+import {Raleway, Roboto} from "next/font/google";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const raleway = Raleway({
+  style: ['normal'],
+  subsets: ['cyrillic', 'latin'],
+  fallback: ['open-sans', 'system-ui', 'arial']
+});
+const roboto = Roboto({
+  weight: ['400', '500', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin', 'cyrillic'],
+  fallback: ['open-sans', 'system-ui', 'arial']
+});
+
+export default function App({Component, pageProps}: AppProps) {
+    return <>
+      {/*<style jsx global>{`*/}
+      {/*  html {*/}
+      {/*    font-family: ${roboto.style.fontFamily};*/}
+      {/*  }*/}
+
+      {/*  h1, h2, h3, h4, h5, h6 {*/}
+      {/*    font-family: ${raleway.style.fontFamily};*/}
+      {/*    margin-bottom: 0.5em;*/}
+      {/*  }*/}
+      {/*`}</style>*/}
+      <Component {...pageProps} />
+    </>
 }
