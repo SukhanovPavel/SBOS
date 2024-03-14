6a0549658e4f2cd05947b9938bc361db04d2d029
Pavel Sukhanov <psuhanov77@gmail.com>
2024-03-14 13:43:05 +0300

    fix: delete imports

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,16 +1,9 @@
 import cx from "classnames";
 import Link from "next/link";
 import {useState} from "react";
-// import { Inter } from 'next/font/google';
 
 import styles from "./Menu.module.css";
 import {ScrollButton} from "@/components/ScrollButton/ScrollButton";
-
-// const inter = Inter({ subsets: ['latin'] });
-
-const ARROW = <svg className={styles.menuIcons} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512">
-    <path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0
-    256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg>
 
 const BURGER =  <svg className={styles.burgerIcon} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
     <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
@@ -25,9 +18,7 @@ const CAMERA = <svg
         version="1.1"
         id="Capa_1"
         xmlns="http://www.w3.org/2000/svg"
-        // xmlns:xlink="http://www.w3.org/1999/xlink"
         viewBox="0 0 51.661 51.661"
-        // xml:space="preserve"
     >
         <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
         <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
@@ -112,16 +103,6 @@ export const Menu = () => {
                 <Link className={styles.card} href="/news">
                     Новости
                 </Link>
-
-                {/*<Link className={styles.card} href="/ourworks" >*/}
-                {/*    <p>Наши работы</p>*/}
-                {/*    <span>{ARROW}</span>*/}
-                {/*</Link>*/}
-
-                {/*<Link className={styles.card} href="/sets">*/}
-                {/*    <p>Комплекты</p>*/}
-                {/*    <span>{ARROW}</span>*/}
-                {/*</Link>*/}
 
                 <Link className={styles.card} href="/contacts">
                     Контакты
