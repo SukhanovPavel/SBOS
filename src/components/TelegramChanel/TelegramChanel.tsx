e3e7269c9b73e74b713b0f897eeef75ff7f1649a
Pavel Sukhanov <psuhanov77@gmail.com>
2024-04-25 15:53:24 +0300

    add news

--- src/components/TelegramChanel/TelegramChanel.tsx
+++ src/components/TelegramChanel/TelegramChanel.tsx
@@ -6,13 +6,13 @@ import styles from "./TelegramChanel.module.css";
 
 const TelegramChannel = () => {
 
-    const arr = ['18', '17', '16', '15'];
+    const arr = ['23', '22', '21', '20'];
 
     return (
         <div className={styles._}>
             <h3>Новости из нашего <Link href="https://t.me/sbosshatura">телеграм канала</Link> :</h3>
 
-            {arr.map((value) => (<>
+            {arr.map((value) => (<div key={value}>
                 <Script
                     src="https://telegram.org/js/telegram-widget.js?22"
                     data-telegram-post={`sbosshatura/${value}`}
@@ -31,7 +31,7 @@ const TelegramChannel = () => {
                         height: "600px"
                     }}>
                 </iframe>
-            </>))}
+            </div>))}
 
             {/*<Script*/}
             {/*    src="https://telegram.org/js/telegram-widget.js?22"*/}
